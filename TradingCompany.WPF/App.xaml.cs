4f732fa8c0a8d294a3d2b2d7d8f6e085cfd6f4a5
olyarytsar <olyarytsar7@gmail.com>
2025-12-03 17:22:30 +0200

    add

--- TradingCompany.WPF/App.xaml.cs
+++ TradingCompany.WPF/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Configuration; 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.IO; 
 using System.Windows;
 using TradingCompany.BLL.Concrete;
 using TradingCompany.BLL.Interfaces;
@@ -15,27 +17,34 @@ namespace TradingCompany.WPF
     public partial class App : Application
     {
         public static IServiceProvider? Services { get; private set; }
-        private const string ConnectionString = "Data Source=localhost,1433;Database=Trading Company;Persist Security Info=True;User ID=sa;Password=MyStr0ng!Pass123;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;";
+
+    
 
         protected override void OnStartup(StartupEventArgs e)
         {
             Services = BuildServiceProvider();
             Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
 
-            
             var mainWindow = Services.GetRequiredService<MainWindow>();
-            
             mainWindow.DataContext = Services.GetRequiredService<MainViewModel>();
-
             mainWindow.Show();
         }
 
         private static IServiceProvider BuildServiceProvider()
         {
+            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) 
+                .AddJsonFile("appsettingss.json", optional: false, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+
             var services = new ServiceCollection();
             services.AddLogging(builder => { builder.AddConsole(); });
 
-          
             services.AddAutoMapper(cfg =>
             {
                 cfg.CreateMap<TradingCompany.DALEF.Models.Role, Role>().ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role1)).ReverseMap();
@@ -47,25 +56,22 @@ namespace TradingCompany.WPF
                 cfg.CreateMap<TradingCompany.DALEF.Models.OrderItem, OrderItem>().ReverseMap();
             });
 
-          
-            services.AddTransient<IEmployeeDAL>(sp => new EmployeeDALEF(ConnectionString, sp.GetRequiredService<IMapper>()));
-            services.AddTransient<IProductDAL>(sp => new ProductDALEF(ConnectionString, sp.GetRequiredService<IMapper>()));
-            services.AddTransient<ISupplierDAL>(sp => new SupplierDALEF(ConnectionString, sp.GetRequiredService<IMapper>()));
-            services.AddTransient<IOrderDAL>(sp => new OrderDALEF(ConnectionString, sp.GetRequiredService<IMapper>()));
-            services.AddTransient<IOrderItemDAL>(sp => new OrderItemDALEF(ConnectionString, sp.GetRequiredService<IMapper>()));
+            services.AddTransient<IEmployeeDAL>(sp => new EmployeeDALEF(connectionString, sp.GetRequiredService<IMapper>()));
+            services.AddTransient<IProductDAL>(sp => new ProductDALEF(connectionString, sp.GetRequiredService<IMapper>()));
+            services.AddTransient<ISupplierDAL>(sp => new SupplierDALEF(connectionString, sp.GetRequiredService<IMapper>()));
+            services.AddTransient<IOrderDAL>(sp => new OrderDALEF(connectionString, sp.GetRequiredService<IMapper>()));
+            services.AddTransient<IOrderItemDAL>(sp => new OrderItemDALEF(connectionString, sp.GetRequiredService<IMapper>()));
 
             services.AddTransient<IAuthManager, AuthManager>();
             services.AddTransient<IProductManager, ProductManager>();
             services.AddTransient<ISupplyManager, SupplyManager>();
 
-         
             services.AddTransient<LoginViewModel>();
-            services.AddTransient<MainViewModel>(); 
+            services.AddTransient<MainViewModel>();
             services.AddTransient<WarehouseManagerViewModel>();
             services.AddTransient<CreateOrderViewModel>();
             services.AddTransient<ActiveOrdersViewModel>();
 
-            
             services.AddTransient<MainWindow>();
 
             return services.BuildServiceProvider();
